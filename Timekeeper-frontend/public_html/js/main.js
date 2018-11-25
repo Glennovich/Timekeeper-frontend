5c20a188957952dc7bef8bf7d9a3eb1c5f436c84
Glennovich <>
2018-11-25 18:00:55 +0100

    - index.html: since this is the page netbeans opens when you go testing, I've added a link to the projects page and one to the tasks page (saves you some typing when you want to go testing) - projects.html: added a div class="container" tag to make sure everything isn't stuck to the sides of the screen and renamed the table "projects" to "tblProjects" to avoid confusion with any other elements - since main.js will be used by multiple pages (or I suspect it will), I've added code to only execute the getProjects(displayProjects) if the table "tblProjects" exists. - added new page tasks.html with a form that can be used to add tasks - select element with projects is dynamically filled from the database. The code to do this will only be run if the select element is present on the page. - saving tasks to the database works and the status code of the request is shown next to the add button after it has been clicked. On success the status code will remain visible for 3 seconds and then disappear, so you know the form is ready for the next input. On failure however, the status code remains visible until the user tries to send data again.

--- Timekeeper-frontend/public_html/js/main.js
+++ Timekeeper-frontend/public_html/js/main.js
@@ -1,9 +1,20 @@
 const backendBaseUrl = httpRequestParamaters.backendUrlDev;
 
 $(document).ready(function () {
-    //init data
-    getProjects(displayProjects);
-
+    //GDG20181125: sorry Steven, but if main.js is to be used on every page, we can't have this be called all the time
+    if($("#tblProjects").length > 0){
+        //init data
+        getProjects(displayProjects);
+    }
+    
+    //if selectbox taskProjectId exists, fill it with projects
+    if($("#taskProjectId").length > 0){
+        //gets projects wrapped in options tags and appends them to our projects selectbox
+        getProjectsAsOptions($("#taskProjectId"));
+    }
+    
+    //materializecss: change select fields
+    $("select").formSelect();
 
     //event listeners
     $("#saveProject").on("click", function(){
@@ -21,6 +32,28 @@ $(document).ready(function () {
             data: formData,
             contentType: "application/json; charset=utf-8",
           });
+    });
+    
+    $("#saveTask").on("click", function(){
+        //hide any previous errors that might still be showing
+        hideTaskFeedback();
+        
+        var formData = {
+            "name": $("#taskName").val(),
+            "description": $("#taskDescription").val(),
+            "projectId": $("#taskProjectId").val()
+        };
+
+        formData = JSON.stringify(formData);
+
+        $.ajax({
+            type: "POST",
+            url: backendBaseUrl + httpRequestParamaters.backendUrlTask,
+            data: formData,
+            contentType: "application/json; charset=utf-8",
+            success: taskSaveSuccess,
+            error: taskSaveError
+          });
     })
 });
 
@@ -33,6 +66,43 @@ function getProjects(cb) {
 
 function displayProjects(data) {
     $.each(data, function (id, project) {
-        $("#projects tbody").append("<tr><td>" + project.name + "</td><td>" + project.description + "</td></tr>");
+        $("#tblProjects tbody").append("<tr><td>" + project.name + "</td><td>" + project.description + "</td></tr>");
     });
+}
+
+function getProjectsAsOptions(selectelement){
+    $.get(backendBaseUrl + httpRequestParamaters.backendUrlProjects, function (data) {
+        $.each(data, function (id, project) {
+            $(selectelement).append("<option value=\"" + project.id + "\">" + project.description + "</option>");
+        });
+        $(selectelement).formSelect();//must be done after dynamically adding option elements or lay-out will suck
+    });
+}
+
+function taskSaveSuccess(data, textStatus, jqXHR){
+    /*
+    console.log("taskSaveSuccess, following 3 lines are data - textStatus - jqXHR");
+    console.log(data);
+    console.log(textStatus);
+    console.log(jqXHR);
+    */
+    $("#taskFeedback").text("status: " + textStatus + " " + jqXHR.status);
+    $("#taskFeedback").addClass("scale-in");
+    //binnen 3 seconden weer weg doen
+    setTimeout(hideTaskFeedback,3000);
+}
+function taskSaveError(jqXHR, textStatus, errorThrown){
+    /*
+    console.log("taskSaveError, following 3 lines are jqXHR - textStatus - errorThrown");
+    console.log(jqXHR);
+    console.log(textStatus);
+    console.log(errorThrown);
+    */
+    $("#taskFeedback").text("status: " + textStatus + " " + jqXHR.status);
+    $("#taskFeedback").addClass("scale-in");
+    //we don't hide errors, we wait until next submit to do that
+}
+function hideTaskFeedback(){
+    $("#taskFeedback").removeClass("scale-in");
+    $("#taskFeedback").text("");
 }
