0e22d2926e1720468fff0119a82a85a3e6012c15
Glennovich <>
2018-11-25 19:30:44 +0100

    Added livetester: does a REST call to /live every 60 seconds to check if the service is still online or not and logs results in a table

--- Timekeeper-frontend/public_html/js/livetester.js
+++ Timekeeper-frontend/public_html/js/livetester.js
@@ -5,9 +5,9 @@ $(document).ready(function () {
 function checkServiceLive(){
     var today = new Date();
     var datetime = today.getFullYear() + "/" + (today.getMonth()+1) + "/" + today.getDate() + " " + 
-            (today.getHours()<10?"0"+today.getHours():today.getHours()) + ":" + 
-            (today.getMinutes()<10?"0"+today.getMinutes():today.getMinutes()) + ":" + 
-            (today.getSeconds()<10?"0"+today.getSeconds():today.getSeconds());
+        (today.getHours()<10?"0"+today.getHours():today.getHours()) + ":" + 
+        (today.getMinutes()<10?"0"+today.getMinutes():today.getMinutes()) + ":" + 
+        (today.getSeconds()<10?"0"+today.getSeconds():today.getSeconds());
     var liveStatus = "";
     $.get(backendBaseUrl + "live", function (data) {
        liveStatus = data.status;
