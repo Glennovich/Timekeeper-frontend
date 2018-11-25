c1e7b4af55e15a3573cec685b5c895fb4c39c482
steven <steven@bimsoft.be>
2018-11-25 20:51:04 +0100

    changes to project overview and project add

--- Timekeeper-frontend/public_html/js/project.js
+++ Timekeeper-frontend/public_html/js/project.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
     $("#saveProject").on("click", function () {
         var formData = {
             "name": $("#projectName").val(),
-            "description": $("#description").val()
+            "description": $("#projectDescription").val()
         };
 
         formData = JSON.stringify(formData);
@@ -18,20 +18,37 @@ $(document).ready(function () {
             url: backendBaseUrl + httpRequestParamaters.backendUrlProjects,
             data: formData,
             contentType: "application/json; charset=utf-8",
+            success: projectSaveSuccess,
+            error: projectSaveError
         });
     });
+
+    $("#addProjectModalTrigger").on("click", function(){
+        $("#addProjectModal").modal();
+    })
 });
 
 function getProjects(cb) {
-    console.log("get");
     $.get(backendBaseUrl + httpRequestParamaters.backendUrlProjects, function (data) {
         cb(data);
     });
 }
 
 function displayProjects(data) {
-    console.log(data);
     $.each(data, function (id, project) {
         $("#tblProjects tbody").append("<tr><td>" + project.name + "</td><td>" + project.description + "</td></tr>");
     });
+}
+
+function projectSaveSuccess(data, textStatus, jqXHR){
+    $('#addProjectModal').modal('close');
+    $("#snackbar").addClass("show");
+
+    setTimeout(() => {
+        $("#snackbar").removeClass("show");
+    }, 3000);
+}
+
+function projectSaveError(jqXHR, textStatus, errorThrown){
+    $("#projectAddFailedMessage").show();
 }
