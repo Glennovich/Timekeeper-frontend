f9c2bd933882263dc56ae22af8777377e00310c2
fahmei <felecia1>
2019-01-06 18:32:20 +0100

    Column number of tasks was ommited in previous security commit

--- Timekeeper-frontend/public_html/js/task.js
+++ Timekeeper-frontend/public_html/js/task.js
@@ -39,7 +39,7 @@ function initializeEventHandlers() {
         M.updateTextFields();
 
         //remove invalid class if needed and set focus on taskName
-        $("#taskName").removeClass("invalid");
+        $("#taskName").val("").removeClass("invalid");
         setTimeout(() => {
             $("#taskName").focus();
         }, 3);
@@ -54,6 +54,9 @@ function initializeEventHandlers() {
 
     $("#taskProjectId").on("change", function () {
         //save current projectId
+        timekeeperStorage.setItem("taskProjectId", $("#taskProjectId").val());
+
+        getTasksForProject($("#taskProjectId").val(), displayTasks);
         updateTaskProject();
     });
 }
@@ -106,9 +109,11 @@ function updateTaskProject() {
     $("#taskProject").val($("#taskProjectId option:selected").text());
 }
 
-        timekeeperStorage.setItem("taskProjectId", $("#taskProjectId").val());
-
-        getTasksForProject($("#taskProjectId").val(), displayTasks);
+function getTasksForProject(projectId, cb) {
+    var url = backendBaseUrl + httpRequestParamaters.backendUrlTasksFromProject + "/" + projectId;
+    get(url, function (result) {
+        $("#addProjectModalTrigger").removeClass("disabled");
+        cb(JSON.parse(result));
     });
 }
 
@@ -122,26 +127,6 @@ function displayTasks(data) {
                 .append("<td>" + task.status + "</td>")
                 .append("<td>" + task.priority + "</td>");
     });
-    //add message to show when there are no projects
-function getTasksForProject(projectId, cb) {
-    var url = backendBaseUrl + httpRequestParamaters.backendUrlTasksFromProject + "/" + projectId;
-    get(url, function (result) {
-        $("#addProjectModalTrigger").removeClass("disabled");
-        cb(JSON.parse(result));
-        $("#tblTasks tbody").append("<tr><td>No tasks found for this project</td><td></td><td></td><td></td></tr>");
-    }
-}
-
-function clearTaskList() {
-    $("#tblTasks tbody tr").remove();
-}
-
-    //clear fields (don't clear taskProject input field, we need that if they want to add a next task)
-    $("#taskId").val("");
-    $("#taskName").val("");
-    $("#taskDescription").val("");
-    $("#taskStatus").val(defaultTaskStatus);
-    $("#taskPriority").val(defaultTaskPriority);
 
     //add event for taskrow.click
     $(".taskRow").on("click", function () {
@@ -164,14 +149,129 @@ function clearTaskList() {
         $("#taskName").removeClass("invalid").focus();
     });
 
+    //add message to show when there are no projects
+    if ($("#tblTasks tbody tr").length == 0) {
+        $("#tblTasks tbody").append("<tr><td>No tasks found for this project</td><td></td><td></td><td></td></tr>");
+    }
+}
+
+function clearTaskList() {
+    $("#tblTasks tbody tr").remove();
+}
+
+function clearFormAddTask() {
+    //clear fields (don't clear taskProject input field, we need that if they want to add a next task)
+    $("#taskId").val("");
+    $("#taskName").val("");
+    $("#taskDescription").val("");
+    $("#taskStatus").val(defaultTaskStatus);
+    $("#taskPriority").val(defaultTaskPriority);
+
     //clear any possible errors
     $("#taskAddFailedMessage").text("");
 }
 
+function getStatusesAsOptions() {
+    var url = backendBaseUrl + httpRequestParamaters.backendUrlStatuses;
+    get(url, setStatussesInSelect, getStatussesError);
+}
+
+function setStatussesInSelect(response) {
+    var selectelement = $("#taskStatus");
+    $.each(JSON.parse(response).taskStatuses, function (id, taskstatus) {
+        $(selectelement).append(new Option(taskstatus.name, taskstatus.name));
+    });
+
+    //if statuses found, enable add button and else select default status
+    if ($(selectelement.selector + " option").length > 0) {
+        $("#addTaskModalTrigger").removeClass("disabled");
+        $(selectelement).val(defaultTaskStatus);
+    }
+
+    $(selectelement).formSelect();
+}
+
+function getStatussesError() {
+    //getting projects failed: disable add button
+    $("#addTaskModalTrigger").addClass("disabled");
+    //NOTE: in case projects have been loaded and the service dies then, 
+    //      clicking on the Save button will still catch the error
+}
+
+function getPrioritiesAsOptions(selectelement) {
+    var url = backendBaseUrl + httpRequestParamaters.backendUrlPriorities;
+    get(url, setPrioritiesInSelect, getPrioritiesError);
+}
+
+function setPrioritiesInSelect(response) {
+    var selectelement = $("#taskPriority");
+    $.each(JSON.parse(response).projectStatuses, function (id, priority) {
+        $(selectelement).append(new Option(priority.name, priority.name));
+    });
+
+    //if priorities found, enable add button and else select default priority
+    if ($(selectelement.selector + " option").length > 0) {
+        $("#addTaskModalTrigger").removeClass("disabled");
+        $(selectelement).val(defaultTaskPriority); //TODO
+    }
+
+    $(selectelement).formSelect();
+}
+
+function getPrioritiesError() {
+    //getting projects failed: disable add button
+    $("#addTaskModalTrigger").addClass("disabled");
+    //NOTE: in case projects have been loaded and the service dies then, 
+    //      clicking on the Save button will still catch the error
+}
+
+function saveTask() {
+    if ($.trim($("#taskName").val()) != "") {
+        if ($.trim($("#taskId").val()) != "") {
+            //edit existing task
+            var d = new Date();
+            var currentTime = d.getFullYear() + "-" + prependZeroes((d.getMonth() + 1), 2) + "-" + prependZeroes(d.getDate(), 2)
+                    + "T" + prependZeroes(d.getHours(), 2) + ":" + prependZeroes(d.getMinutes(), 2) + ":00";
+            var formData = {
+                "id": $("#taskId").val(),
+                "name": $("#taskName").val(),
+                "description": $("#taskDescription").val(),
+                "projectId": $("#taskProjectId").val(),
+                "status": $("#taskStatus").val(),
+                "priority": $("#taskPriority").val(),
+                "currentTime": currentTime
+            };
+            var url = backendBaseUrl + httpRequestParamaters.backendUrlTasks
+            put(url, formData, taskEditSuccess, taskEditError);
+
+        } else {
+            //add new
+            var formData = {
+                "name": $("#taskName").val(),
+                "description": $("#taskDescription").val(),
+                "projectId": $("#taskProjectId").val(),
+                "status": $("#taskStatus").val(),
+                "priority": $("#taskPriority").val()
+            };
+            var url = backendBaseUrl + httpRequestParamaters.backendUrlTasks
+            post(url, formData, taskAddSuccess, taskAddError);
         }
     }
 }
 
+function prependZeroes(number, maxlength) {
+    return number.toString().length < maxlength ? times("0", maxlength - number.toString().length) + number : number;
+}
+
+function times(dink, numberOfTimes) {
+    var strTimes = "";
+    for (var i = 0; i < numberOfTimes; i++) {
+        strTimes += dink;
+    }
+    return strTimes;
+}
+
+function taskAddSuccess() {
     clearFormAddTask();
 
     $("#addTaskModal").modal("close");
@@ -185,7 +285,7 @@ function clearTaskList() {
     }, 100);
 }
 
-    if ($("#tblTasks tbody tr").length == 0) {
+function taskAddError(response) {
     //if we have an error: show it, otherwise: default error
     if (response) {
         $("#taskAddFailedMessage").text(response.responseText).show();
@@ -194,4 +294,31 @@ function clearTaskList() {
     }
 }
 
-function clearFormAddTask() {
+function taskEditSuccess() {
+    clearFormAddTask();
+    $("#addTaskModal").modal("close");
+
+    //show a snackbar to let the user know that the task is created successfully
+    snackbar("Task changed!");
+
+    //reload row for changed task
+    setTimeout(() => {
+        getTasksForProject($("#taskProjectId").val(), displayTasks);
+    }, 100);
+}
+
+function taskEditError(response) {
+    //if we have an error: show it, otherwise: default error
+    if (response) {
+        $("#taskAddFailedMessage").text(response.responseText).show();
+    } else {
+        $("#taskAddFailedMessage").text("Task could not be changed, please try again later!").show();
+    }
+}
+
+function snackbar(text) {
+    $("#snackbar").text(text).addClass("show");
+    setTimeout(() => {
+        $("#snackbar").removeClass("show");
+    }, 3000);
+}
