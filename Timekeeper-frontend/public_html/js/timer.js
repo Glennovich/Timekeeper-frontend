b026a37ed85367d326e8c7fadc3564b8bd94a905
steven <steven@bimsoft.be>
2019-02-07 21:49:45 +0100

    disable menu when timer starts

--- Timekeeper-frontend/public_html/js/timer.js
+++ Timekeeper-frontend/public_html/js/timer.js
@@ -4,15 +4,21 @@ var seconds = 0, minutes = 0, hours = 0;
 $(document).ready(function(){
     //event listeners
     $("#startTimer").on("click", function(){
+        //disable menu
+        disableMenu();
         t = setTimeout(timer, 1000);
     });
 
     $("#stopTimer").on("click", function(){
+        //enable menu
+        enableMenu();
         clearTimeout(t);
     });
 
     //set the timer back to 00:00:00
     $("#resetTimer").on("click", function(){
+        //enable menu
+        enableMenu();
         clearTimeout(t);
         seconds = 0;
         minutes = 0;
@@ -35,6 +41,20 @@ function timer() {
     $("#timer").text(formatTimer(hours, minutes, seconds));
 
     t = setTimeout(timer, 1000);
+}
+
+function disableMenu(){
+    //disable head menu
+    $(".nav-wrapper ul").addClass("isDisabled");
+    //disable sidenav for mobile
+    $(".sidenav").addClass("isDisabled");
+}
+
+function enableMenu(){
+    //enable head menu
+    var nav = $(".nav-wrapper ul").removeClass("isDisabled");
+    //enable sidenav for mobile
+    var sideNav = $(".sidenav").removeClass("isDisabled");
 }
 
 function formatTimer(hours, minutes, seconds){
