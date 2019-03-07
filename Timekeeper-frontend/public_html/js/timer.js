edf44630b7c20bf0a79fc3f110e120465d1e4829
fahmei <felecia1>
2019-03-07 23:46:24 +0100

    TIMEKEEPER-126 Menu bar is disabled when pressing play Menu bar is enabled when pressing stop Reset and Stop are disabled until play is pressed Play is disabled when pressed Reset is only on if the timer has played Reset is disabled when pressed

--- Timekeeper-frontend/public_html/js/timer.js
+++ Timekeeper-frontend/public_html/js/timer.js
@@ -2,36 +2,38 @@ var t;
 var seconds = 0, minutes = 0, hours = 0;
 var isRunning = false;
 
-$(document).ready(function(){
-    //event listeners
-    $("#btnStartTimer").on("click", function(){
-        if(!isRunning){
-        //disable menu
+$(document).ready(function () {
+    $("#btnStartTimer").on("click", function () {
+        if (!isRunning) {
             disableMenu();
+            disableElement($("#btnStartTimer"), true);
+            disableElement($("#btnStopTimer"), false);
+            disableElement($("#btnResetTimer"), true);
             isRunning = true;
             t = setTimeout(updateTimer, 1000);
         }
     });
 
-    $("#btnStopTimer").on("click", function(){
-        if(isRunning){
-            //enable menu
+    $("#btnStopTimer").on("click", function () {
+        if (isRunning) {
             enableMenu();
+            disableElement($("#btnStartTimer"), false);
+            disableElement($("#btnStopTimer"), true);
+            disableElement($("#btnResetTimer"), false);
             isRunning = false;
             clearTimeout(t);
         }
     });
 
-    //set the timer back to 00:00:00
-    $("#btnResetTimer").on("click", function(){
-            //enable menu
-            enableMenu();
-            isRunning = false;
-            clearTimeout(t);
-            seconds = 0;
-            minutes = 0;
-            hours = 0;
-            $("#timer").text("00:00:00");
+    $("#btnResetTimer").on("click", function () {
+        enableMenu();
+        disableElement($("#btnResetTimer"), true);
+        isRunning = false;
+        clearTimeout(t);
+        seconds = 0;
+        minutes = 0;
+        hours = 0;
+        $("#timer").text("00:00:00");
     });
 });
 
@@ -45,26 +47,26 @@ function updateTimer() {
             hours++;
         }
     }
-    
+
     $("#timer").text(formatTimer(hours, minutes, seconds));
 
     t = setTimeout(updateTimer, 1000);
 }
 
-function disableMenu(){
-    disableElement($(".menu-bar"), true);
-    //disable head menu
-//    $(".menu-bar a").foreach;
+function disableMenu() {
+    $.each($(".menu-bar *"), function (i, el) {
+        disableElement(el, true);
+    });
 }
 
-function enableMenu(){
-    disableElement($(".menu-bar"), true);
-    //enable head menu
-//    var nav = $(".nav-wrapper ul").removeClass("isDisabled");
+function enableMenu() {
+    $.each($(".menu-bar *"), function (i, el) {
+        disableElement(el, false);
+    });
 }
 
-function formatTimer(hours, minutes, seconds){
-    return (hours ? (hours > 9 ? hours : "0" + hours) : "00") + 
-    ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + 
-    ":" + (seconds > 9 ? seconds : "0" + seconds);
+function formatTimer(hours, minutes, seconds) {
+    return (hours ? (hours > 9 ? hours : "0" + hours) : "00") +
+            ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") +
+            ":" + (seconds > 9 ? seconds : "0" + seconds);
 }
